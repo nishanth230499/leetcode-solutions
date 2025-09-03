9a9e567d6240941dc724386ba507756c5304bef6
nishanth-ucr <npras017@ucr.edu>
2025-09-03 12:17:38 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+| Question                                                                  | Solutions                                   |
+| :------------------------------------------------------------------------ | :------------------------------------------ |
+| [257. Binary Tree Paths](https://leetcode.com/problems/binary-tree-paths) | [python3](python3/253-binary-tree-paths.py) |
---
+++ python3/253-binary-tree-paths.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        res = []
+        def rec(cur, pre_res):
+            if cur.left == None and cur.right == None:
+                res.append(pre_res + [str(cur.val)])
+            
+            if cur.left:
+                rec(cur.left, pre_res + [str(cur.val)])
+            
+            if cur.right:
+                rec(cur.right, pre_res + [str(cur.val)])
+        
+        rec(root, [])
+
+        return list(map(lambda a: "->".join(a), res))
